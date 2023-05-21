de11316ab48a0db13fb9756fe6f492703c563d9d
luis barri <luisbarriw@gmail.com>
2023-05-21 09:29:06 +0200

    layout and structure - verkstad och simulator

--- src/app/simulator/page.tsx
+++ src/app/simulator/page.tsx
@@ -1,33 +1,51 @@
 import React from "react";
-
 import "./simulator.css";
 
 const Simulator = () => {
   return (
-    <div>
-      <div className="simulator__section1-container">
-        <img className="simulator__section1-image" src="heli5.png" alt=""></img>
-      </div>
-      <div className="simulator__section1-infocontainer">
-        <h1>SIMULATOR</h1>
-        <h3>
-          Proflight har en av de få fullskaliga helikoptersimulatorerna i norra
-          Europa. Användningsområdet för simulatorn är stort och sträcker sig
-          hela vägen från komplicerade instrumentflygutbildningar och MCC hela
-          vägen ned till mörkerträning och rena navigationsövningar. Simulatorn
-          fyller två primära funktioner. Den viktigaste är möjligheten att träna
-          och öva på situationer som är svåra att skapa i verkligheten utan att
-          framkalla verklig fara. Den sekundära är att kostnaden för att flyga
-          simulatorn är lägre vilket medför mer flygtid och att den blivande
-          piloten därigenom får avsevärt mycket mer rutin och träning i svåra
-          scenarion. På detta sätt hjälper simulatorn, med sina mycket
-          välrenommerade instruktörer, till att skapa bättre piloter som är
-          tryggare i sina arbetssituationer. Vi erbjuder även allmänheten
-          möjligheten att prova på att flyga en helikopter i denna riskfria
-          miljö. En uppskattad upplevelse. Läs mer om vårt simulator för
-          alllmänheten under vårt presentpaket. Kontakta oss på Proflight så
-          berättar vi mer.
-        </h3>
+    <div className="simulator__container">
+      <div>
+        <div className="simulator__background"></div>
+        <div className="simulator__infocontainer">
+          <h1 className="simulator__main-title">Simulator</h1>
+          <div className="simulator__main-subtitle">
+            Proflight har en av de få fullskaliga helikoptersimulatorerna i
+            norra Europa. Användningsområdet för simulatorn är stort och
+            sträcker sig hela vägen från komplicerade instrumentflygutbildningar
+            och MCC hela vägen ned till mörkerträning och rena
+            navigationsövningar. Vill du veta mer?
+          </div>
+
+          <button className="simulator__button">
+            {" "}
+            Kontakta oss på Proflight{" "}
+          </button>
+          <h3 className="simulator__subtitle-container">
+            <div className="simulator__subtitle-innercontainer">
+              <div className="simulator__title">Två primära funktioner</div>
+              <div className="simulator__subtitle">
+                Simulatorn fyller två primära funktioner. Den viktigaste är
+                möjligheten att träna och öva på situationer som är svåra att
+                skapa i verkligheten utan att framkalla verklig fara. Den
+                sekundära är att kostnaden för att flyga simulatorn är lägre
+                vilket medför mer flygtid och att den blivande piloten därigenom
+                får avsevärt mycket mer rutin och träning i svåra scenarion.
+              </div>
+            </div>
+            <div className="simulator__subtitle-innercontainer">
+              <div className="simulator__title">Bättre piloter</div>
+              <div className="simulator__subtitle">
+                På detta sätt hjälper simulatorn, med sina mycket välrenommerade
+                instruktörer, till att skapa bättre piloter som är tryggare i
+                sina arbetssituationer. Vi erbjuder även allmänheten möjligheten
+                att prova på att flyga en helikopter i denna riskfria miljö. En
+                uppskattad upplevelse. Läs mer om vårt simulator för
+                alllmänheten under vårt presentpaket.
+              </div>
+            </div>
+          </h3>
+        </div>
+        <img className="simulator__image" src="simulator2.jpg" alt=""></img>
       </div>
     </div>
   );
--- src/app/verkstad/page.tsx
+++ src/app/verkstad/page.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import "../simulator/simulator.css";
 
 const Verkstad = () => {
-  return <div>page</div>;
+  return (
+    <div className="simulator__container">
+      <div>
+        <div className="simulator__background"></div>
+        <div className="simulator__infocontainer">
+          <h1 className="simulator__main-title">Verkstad</h1>
+          <div className="simulator__main-subtitle">
+            Proflight tillhandahåller en komplett lösning för underhåll av
+            helikoptrar. Vi har samtliga tillstånd av Transportstyrelsen för att
+            se till att er helikopter förblir luftvärdig. Vi kan utföra alla
+            tänkbara jobb, allt från en 100-timmars tillsyn till lackering, byte
+            av inredning samt uppgradering av motor. Vi har många
+            referensobjekt. Kort sagt, har du en helikopter, då har vi en
+            lösning!
+          </div>
+          <h3 className="simulator__subtitle-container">
+            <div className="simulator__subtitle-innercontainer">
+              <div className="simulator__title">
+                Continuing Airworthiness Management Organisation (CAMO)
+              </div>
+              <div className="simulator__subtitle">
+                Organisation som ser till att fortlöpande kontrollera samtliga
+                komponenters gångtider, bulletiner från tillverkarna samt
+                Airworthiness Directives från myndigheterna. I normalfallet bär
+                ägaren hela luftvärdighetsansvaret men genom ett sk. CAMO-avtal
+                kan detta ansvar överlåtas.
+              </div>
+            </div>
+            <div className="simulator__subtitle-innercontainer">
+              <div className="simulator__title">Airworthiness Review</div>
+              <div className="simulator__subtitle">
+                Som ett bevis på vår långa, gedigna erfarenhet så har vi
+                behörighet att granska luftvärdigheten på luftfartyg. Om vi
+                finner helikoptern luftvärdig, får vi tack vare vår
+                auktorisation, utfärda ARC (Airworthiness Review Certificate)
+                som har erkänd legitimitet inom hela Europa.
+              </div>
+            </div>
+            <div className="simulator__subtitle-innercontainer">
+              <div className="simulator__title">Part-145</div>
+              <div className="simulator__subtitle">
+                Musklerna i allt underhåll, här sker den fysiska
+                handpåläggningen. Verkstan utför sina underhållsåtgärder med
+                certifierade tekniker, delar som kan spåras i varje led av
+                tillverkningen samt med de senaste manualerna.
+              </div>
+            </div>
+          </h3>
+        </div>
+        <img className="simulator__image" src="verkstad.jpeg" alt=""></img>
+      </div>
+    </div>
+  );
 };
 
 export default Verkstad;
